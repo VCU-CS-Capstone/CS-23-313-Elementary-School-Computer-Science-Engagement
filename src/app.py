28c91532fb80d1d24c5af3bc34d346be34a2bf7b
Kofi-kay <kierstenkofif@gmail.com>
2022-11-29 14:38:25 -0500

    updates documentation

--- src/app.py
+++ src/app.py
@@ -10,32 +10,49 @@ app.config['SECRET_KEY'] = 'read my mind'
 def index():
     return render_template('index.html')
 
-@app.route('/home')
+@app.route('/nontech/')
+def nontech():
+    return render_template('nontech.html')
+
+@app.route('/tech/')
+def tech():
+    return render_template('tech.html')
+
+@app.route('/sorting/')
+def sorting():
+    return render_template('sorting.html')
+
+@app.route('/require/')
+def require():
+    return render_template('require.html')
+
+
+@app.route('/home/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/lawncare')
+@app.route('/lawncare/')
 def lawncare():
     return render_template('lawncare.html')
 
 
-@app.route('/calculator')
+@app.route('/calculator/')
 def calculator():
     return render_template('calculator.html')
 
 
-@app.route('/positivenegative')
+@app.route('/positivenegative/')
 def positivenegative():
     return render_template('positivenegative.html')
 
 
-@app.route('/inputoutput')
+@app.route('/inputoutput/')
 def inputoutput():
     return render_template('inputoutput.html')
 
 
-@app.route('/links')
+@app.route('/links/')
 def links():
     return render_template('links.html')
 
